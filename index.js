29aabe998338acf9d610923b1c6a0230fea5b346
tonsojun <josephjun@gmail.com>
2025-08-20 19:00:28 -0700

    First commit, can't get p element to render

---
+++ index.js
@@ -0,0 +1,19 @@
+const noteInput = document.getElementById("new-note");
+
+const addNoteButton = document.getElementById("add-note");
+
+addNoteButton.addEventListener("click", addANewNote);
+
+
+function addANewNote() {
+
+    const noteText = noteInput.value;
+
+    console.log("The text inserted is " + noteText);
+
+    const newNote = document.createElement("p");
+
+    newNote.innerHTML = "Test new paragraph created";
+
+    document.body.appendChild(newNote);
+}
